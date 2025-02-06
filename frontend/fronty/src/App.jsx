35059d31dc9474b8c96a8ae9e2985a13f0037b34
Ved-Kulkarni17 <136963983+Ved-Kulkarni17@users.noreply.github.com>
2025-02-07 03:38:38 +0530

    Update App.jsx
    customisation page added for new users

--- frontend/fronty/src/App.jsx
+++ frontend/fronty/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dash from "./pages/Dash";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
+import Custom from "./pages/Custom"
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Dash" element={<Dash />} />
+        <Route path="/Custom" element={<Custom />} />
       </Routes>
     </Router>
   );
